016e023cbfe130660479d55d836577bd61fa3143
kanakroy0390 <kanak0390@gmail.com>
2023-02-11 22:22:55 +0530

    C:\Users\Lalan\React\React Text Color

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react'
+import '../styles/App.css';
+
+const App = () => {
+//code here 
+  const [style, setStyle] = useState("redColor");
+  const [value, setValue]= useState(true);
+  
+  function changeStyle(){
+    setValue(!value);
+  }
+  useEffect( () => {
+    if(value===false){
+    setStyle("blueColor");
+    }
+    else{
+    setStyle("redColor");
+    }
+  },[value]);
+
+  return (
+    <div id="main">
+      <p className={style} >Newton School</p>
+      <button id='button' onClick={changeStyle}>Change Style</button>
+    </div>
+  )
+}
+
+
+export default App;
